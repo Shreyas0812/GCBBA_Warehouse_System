e35ab0ac1eb3719b0d8ef2b1eb76f210f7a08279
Shreyas Raorane <raorane@seas.upenn.edu>
2026-02-09 19:21:35 -0500

    some variables are not to be made available throughout to avoid confusion later

--- integration/orchestrator.py
+++ integration/orchestrator.py
@@ -34,7 +34,7 @@ class IntegrationOrchestrator:
                  max_plan_time: int = 400,
                  Lt: Optional[int] = None
                  ) -> None:
-        self.config_path = config_path
+        
         self.tasks_per_induct_station = tasks_per_induct_station
         self.comm_range = comm_range
         self.sp_lim = sp_lim
@@ -47,30 +47,32 @@ class IntegrationOrchestrator:
         self.grid_map = GridMap(config_path)
         self.ca = TimeBasedCollisionAvoidance(self.grid_map)
 
-        self._load_config()
-        self._init_gcbba()
+        agent_positions, induct_positions, eject_positions = self._load_config(config_path)
+        self._init_gcbba(agent_positions, induct_positions, eject_positions)
         self._init_agent_states()
 
-    def _load_config(self) -> None:
-        with open(self.config_path, 'r') as f:
+    def _load_config(self, config_path: str) -> Tuple[List, List, List]:
+        with open(config_path, 'r') as f:
             config = yaml.safe_load(f)
         
         params = config["create_gridworld_node"]["ros__parameters"]
 
         agent_pos_flat = params['agent_positions']
-        self.agent_positions = [(agent_pos_flat[i], agent_pos_flat[i+1], agent_pos_flat[i+2], i//3 + 1) 
+        agent_positions = [(agent_pos_flat[i], agent_pos_flat[i+1], agent_pos_flat[i+2], i//3 + 1) 
                                 for i in range(0, len(agent_pos_flat), 3)]
         
         induct_pos_flat = params['induct_stations']
-        self.induct_positions = [(induct_pos_flat[i], induct_pos_flat[i+1], induct_pos_flat[i+2], induct_pos_flat[i+3]) 
+        induct_positions = [(induct_pos_flat[i], induct_pos_flat[i+1], induct_pos_flat[i+2], induct_pos_flat[i+3]) 
                                  for i in range(0, len(induct_pos_flat), 4)]
         
         eject_pos_flat = params['eject_stations']
-        self.eject_positions = [(eject_pos_flat[i], eject_pos_flat[i+1], eject_pos_flat[i+2], eject_pos_flat[i+3]) 
+        eject_positions = [(eject_pos_flat[i], eject_pos_flat[i+1], eject_pos_flat[i+2], eject_pos_flat[i+3]) 
                                 for i in range(0, len(eject_pos_flat), 4)]
 
-    def _init_gcbba(self) -> None:
-        raw_graph, G = create_graph_with_range(self.agent_positions, self.comm_range)
+        return agent_positions, induct_positions, eject_positions
+
+    def _init_gcbba(self, agent_positions: List, induct_positions: List, eject_positions: List) -> None:
+        raw_graph, G = create_graph_with_range(agent_positions, self.comm_range)
         if raw_graph.number_of_nodes() == 0:
             D = 1
         else:
@@ -80,22 +82,22 @@ class IntegrationOrchestrator:
                 # If the graph is not fully connected, we can take the maximum diameter of the connected components as an approximation
                 D = max(nx.diameter(raw_graph.subgraph(c)) for c in nx.connected_components(raw_graph))
 
-        agents = agent_init(self.agent_positions, sp_lim=self.sp_lim)
-        tasks = task_init(self.induct_positions, self.eject_positions, task_per_induct_station=self.tasks_per_induct_station)
+        agents = agent_init(agent_positions, sp_lim=self.sp_lim)
+        tasks = task_init(induct_positions, eject_positions, task_per_induct_station=self.tasks_per_induct_station)
 
         if self.Lt is None:
-            nt = len(self.induct_positions) * self.tasks_per_induct_station
-            na = len(self.agent_positions)
+            nt = len(induct_positions) * self.tasks_per_induct_station
+            na = len(agent_positions)
             Lt = int(np.ceil(nt / na))
         
         self.gcbba_orchestrator = GCBBA_Orchestrator(G, D, tasks, agents, Lt)
 
     def _init_agent_states(self) -> None:
         self.agent_states: List[AgentState] = []
-        for idx, agent_pos in enumerate(self.agent_positions):
-            agent_id = idx
-            grid_pos = self.grid_map.continuous_to_grid(float(agent_pos[:3][0]), float(agent_pos[:3][1]), float(agent_pos[:3][2]))
-            # speed 
+        # for idx, agent_pos in enumerate(self.agent_positions):
+            # agent_id = self.
+            # grid_pos = self.grid_map.continuous_to_grid(float(agent_pos[:3][0]), float(agent_pos[:3][1]), float(agent_pos[:3][2]))
+            # speed = agent_pos[3]
             # self.agent_states.append(AgentState(agent_id, grid_pos))
 
 if __name__ == "__main__":
