862d606e5410a42654dff9d74e878ed23c942687
Shreyas Raorane <raorane@seas.upenn.edu>
2026-02-18 16:39:10 -0500

    added launch agents functionality

--- baselines/CBBA_Orchestrator.py
+++ baselines/CBBA_Orchestrator.py
@@ -56,4 +56,42 @@ class CBBA_Orchestrator:
         for i in range(self.na):
             self.agents.append(CBBA_Agent(id=i, G=self.G, char_a=self.char_a[i], tasks=self.tasks, Lt=self.Lt, start_time=self.start_time, metric=self.metric, D=self.D, grid_map=self.grid_map))
 
+    def launch_agents(self, method=None, detector=None):
+        """
+        Launch CBBA
+
+        - nb_iter = Nmin * D total iterations
+        - nb_consensus = 1 consensus round per iteration
+        - FULLBUNDLE bundle building (agents fill entire bundle before consensus)
+        - no convergence detection
+        """
+
+        D = self.D
+        Nmin = int(min(self.na * self.Lt, self.nt))
+
+        nb_iter = Nmin * D
+        nb_cons = 1
+
+        for iteration in range(nb_iter):
+            # Phase 1: Bundle Building
+            for agent in self.agents:
+                agent.create_bundle()
+            
+            # Phase 2: Consensus (single round)
+            for consensus_num in range(nb_cons):
+                all_agents = [agent.snapshot() for agent in self.agents]
+                consensus_iter = nb_cons * iteration + consensus_num
+
+                for agent in self.agents:
+                    agent.resolve_conflicts(all_agents, consensus_iter=consensus_iter, consensus_index_last=True)
+            
+            assignment, bid, max_time = self.gather_info()
+
+            self.assig_history.append(assignment)
+            self.bid_history.append(bid)
+            self.max_times.append(max_time)
     
+        if len(self.assig_history) > 0:
+            return self.assig_history[-1], self.bid_history[-1], self.max_times[-1]
+        else:
+            return [], 0, 0
